3d0292392aa41c36484aeeed9181a3afd78a41ee
Tango-21 <79048989+Tango-21@users.noreply.github.com>
2021-06-20 20:02:44 +0100

    Squirt score (#3)
    * Update Sqrtr_0.1.1.ino
    
    * Rename Sqrtr_0.1.1.ino to Sqrtr_0.1.2.ino
    
    * Update Sqrtr_0.1.2.ino
    
    * Update README.md
    
    Updates

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Irrigation-System
-This repository is for the code needed to run a home garden irrigation system using an Arduino Uno, Soil Hygrometer (SH), and 12V water pump.
+This repository is for the code needed to run a home garden irrigation system called Squirter. The system uses an Arduino Uno, Soil Hygrometer (SH), and 12V water pump.
 
 Functionality so far:
-- The system takes an analogue reading from the SH, and maps it from 0-1023 -> 0-100% then prints the 'Moisture level' to Serial.
-- 'Moisture level' readings are sequentially saved to a 10 value array, before looping back and overwriting the oldest reading.
+- The system takes an analogue reading from the SH, and maps it from 0-1023 -> 0-100% then prints the 'Moisture Rating' to Serial.
+- 'Moisture Ratings' are sequentially saved to a 10 value array, before looping back and overwriting the oldest reading.
+- Squirt Scores - Use previous 10 readings to determine whether or not to Squirt (trigger the pump/light up led)
 
 Roadmap:
-- Use previous 10 readings to determing whether or not to trigger the pump
-- Pump testing
+
+- Pump testing, using led
+- emergency squirt
 
 Version history:
-0.1 - Take reading from SH, map it to a % value, print reading to Serial.
-0.1.1 - the 10 previous moisture readings are saved to an array. The 11th reading overwrites the first, the 12th overwrites the 2nd etc. 
+- 0.1 - Take reading from SH, map it to a % value, print reading to Serial.
+- 0.1.1 - the 10 previous moisture readings are saved to an array. The 11th reading overwrites the first, the 12th overwrites the 2nd etc. 
+- 0.1.2 - Easily configurable settings. Displays the last 10 readings evrery 10 readings. Introduction of Squirt Scores to determine when to Squirt.
---
+++ Sqrtr_0.1.2.ino
@@ -0,0 +1,142 @@
+int sensorPin = A0;
+int moistureReading;
+int moistureRating;
+  // The reading from the Moisture Detector expressed as a percentage.
+int memory[10];
+int memoryIndex = 0;
+int squirtScore = 0;
+  //The Squirt Score is determined by how many of the last 10 Moisture Ratings has a +1 to Squirt Score.
+
+
+//CONFIG SETTINGS
+int squirtThreshold = 5;
+  //The number of cycles out of the last 10 to activate a Squirt.
+int moistureThreshold = 40;
+  //The value below which a Moisture Reading causes a +1 to Squirt Score.
+int cycleTime = 2000 ;
+  //Time in miliseconds.
+  //The delay between begining cycles. This can be used to control the total length of time between readings & thus minimum time between Squirts.
+int squirtTime = 5000  
+  //Time in miliseconds.
+  //The length of a Squirt.
+
+void setup() {
+
+  Serial.begin(9600);
+  //Establishes communications with the computer. Bitrate of 9600
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  //The LED will light up when the sketch is running.
+  
+  Serial.println("Reading from the sensor...");
+
+  delay(1500)
+  ;
+}
+
+
+void loop() {
+
+//SENSOR READING & STORAGE
+  moistureReading = analogRead(sensorPin);
+  //'reading' is defined as the analogue (0-1023) value from the Sensor
+  moistureRating = map(moistureReading, 1023, 0, 0, 100);
+  
+  memory[memoryIndex] = moistureRating;
+  //The 'moistureRating' value is saved to the 'memoryIndex' position. This starts at 0.
+
+
+//SQUIRT SCORING
+  if(memory[0] > moistureThreshold)
+  {
+ squirtScore++;
+ }
+  if(memory[1] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[2] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[3] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[4] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[5] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[6] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[7] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[8] > moistureThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[9] > moistureThreshold)
+{
+ squirtScore++;
+  } ;
+
+//OUTPUTS - Serial Monitor
+ 
+ Serial.println("============");
+ Serial.print("Memory position: "); Serial.println(1+memoryIndex); 
+    //Probably hide this later.
+ Serial.print("Moisture Rating: "); Serial.print(moistureRating); Serial.println("%");
+  //Print: "Moisture level: XX %" to the serial monitor. 
+ Serial.print("Squirt Score: "); Serial.print(squirtScore); Serial.print("/ 10 (Threshold: "); Serial.print(squirtScore); Serial.println(")");
+ Serial.println("");
+
+if (memoryIndex == 9) {   
+  Serial.println("The last 10 Moisture Ratings:");
+  Serial.print(memory[0]); Serial.println("%");
+  Serial.print(memory[1]); Serial.println("%");
+  Serial.print(memory[2]); Serial.println("%");
+  Serial.print(memory[3]); Serial.println("%");
+  Serial.print(memory[4]); Serial.println("%");
+  Serial.print(memory[5]); Serial.println("%");
+  Serial.print(memory[6]); Serial.println("%");
+  Serial.print(memory[7]); Serial.println("%");
+  Serial.print(memory[8]); Serial.println("%");
+  Serial.print(memory[9]); Serial.println("%");
+}
+
+
+//OUTPUTS - Squirt mode
+//Code to follow. LED initially, pump eventually
+//if(squirtScore >= scquirtThreshold) {
+//digitalWrite(PIN, HIGH);
+//delay(squirtTime);
+//digitalWrite(PIN, LOW);
+//  }
+
+
+//CYCLE & RESET memoryIndex
+ memoryIndex++;
+  //+1 to the 'memoryIndex' value. This puts the next reading in the next memory position.
+
+ if(memoryIndex > 9)
+  {  
+  memoryIndex = 0;
+  }
+  //Resets 'memoryIndex' back to 0 to overwrite the oldest readings.
+ 
+ delay(cycleTime);
+  //Eventually, this should be increased to at least 5 minutes (300000)
+  ;
+}
+
+
+  
