8befa30107896d135bacab239c065f55bf60ca48
Tango-21 <79048989+Tango-21@users.noreply.github.com>
2021-06-20 16:41:30 +0100

    Update Sqrtr_0.1.1.ino

--- Sqrtr_0.1.1.ino
+++ Sqrtr_0.1.1.ino
@@ -3,7 +3,9 @@ int reading;
 int percentage;
 int memory[10];
 int memoryIndex = 0;
-
+int squirtScore = 0;
+int squirtThreshold = 5;
+int percentageThreshold = 50;
 
 void setup() {
 
@@ -28,9 +30,6 @@ void loop() {
 
   percentage = map(reading, 1023, 0, 0, 100);
 
-  memory[memoryIndex] = percentage;
-  //The 'percentage' value is saved to the 'memoryIndex' position. This starts at 0.
-
   if(memoryIndex > 9)
   {
   Serial.println(memory[0]);
@@ -43,11 +42,15 @@ void loop() {
   Serial.println(memory[7]);
   Serial.println(memory[8]);
   Serial.println(memory[9]);
-    //Print the memory array - Remove this for live version
+   //Print the memory array - Remove this for live version
+  
   memoryIndex = 0;
-    
   }
   //Loops 'memoryIndex' back to 0 to overwrite the oldest readings.
+
+   memory[memoryIndex] = percentage;
+  //The 'percentage' value is saved to the 'memoryIndex' position. This starts at 0.
+
 
   Serial.print("Memory position: "); Serial.println(1+memoryIndex); 
 
@@ -58,6 +61,56 @@ void loop() {
 
 memoryIndex++;
   //+1 to the 'memoryIndex' value. This puts the next reading in the next memory position.
+
+  
+//Below uses the previous 10 readings to determine the squirt score.  
+int squirtScore = 0;
+int squirtThreshold = 5;
+  
+  if(memory[0] > percentageThreshold)
+{
+  {
+ squirtScore++;
+ }
+  if(memory[1] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[2] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[3] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[4] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[5] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[6] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[7] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[8] > percentageThreshold)
+{
+ squirtScore++;
+ }
+  if(memory[9] > percentageThreshold)
+{
+ squirtScore++;
+  }
+} ;
+
+ Serial.print("Squirt Score: "); Serial.print(squirtScore); 
   
   delay(5000);
   //Eventually, this should be increased to at least 5 minutes (300000)
